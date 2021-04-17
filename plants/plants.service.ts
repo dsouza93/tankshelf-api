a34543ebba73c4e68ee762c787d69d632132651f
Joe Flanagan <joe.flan@protonmail.com>
2021-04-17 11:08:43 -0600

    endpoints for grabbing contents of tank

--- plants/plants.service.ts
+++ plants/plants.service.ts
@@ -4,3 +4,11 @@ export const findAll = async(): Promise<any> => {
     const rows = await db.pool.query('SELECT * FROM tankshelf.freshwater_plants');
     return rows[0];
 }
+
+// find plants owned by tankID
+export const findByTankID = async(tankID: number): Promise<any> => {
+    const rows = await db.pool.query('SELECT fp.* FROM tankshelf.tank_contents_plants tcp JOIN tankshelf.freshwater_plants fp ON tcp.plantID = fp.plantID WHERE tcp.tankID = ?',
+        [tankID]);
+        
+    return rows[0];
+}
